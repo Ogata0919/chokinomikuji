949594b2fabbc6d5982250e4f234d1cfb9e6d80d
Ogata0919 <71375611+Ogata0919@users.noreply.github.com>
2021-02-25 12:20:45 +0900

    修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_blog, only: [:edit, :show, :update, :destroy]
   def index
     @blogs = Blog.all
   end
@@ -8,37 +10,41 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def create
-    Blog.create(blog_parameter)
-    redirect_to blogs_path
+    @blog = Blog.new(blog_parameter)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
   end
 
   def destroy
-    @blog = Blog.find(params[:id])
-    @blog.destroy
+    @blog.destroy if current_user.id == @blog.user_id
     redirect_to blogs_path, notice:"削除しました"
   end
 
   def edit
-    @blog = Blog.find(params[:id])
+    return redirect_to blogs_path if current_user.id != @blog.user.id
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_parameter)
       redirect_to blogs_path, notice: "編集しました"
     else
-      render 'edit'
+      render :edit
     end
   end
 
   private
 
   def blog_parameter
-    params.require(:blog).permit(:title, :content, :start_time)
+    params.require(:blog).permit(:title, :content, :start_time).merge(user_id: current_user.id)
   end
 
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,13 @@
 ActiveRecord::Schema.define(version: 2021_02_16_055150) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
     t.datetime "start_time"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_02_16_055150) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
